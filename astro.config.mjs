34b62ef097460781020f077464202fd6b1392757
adrian-burkhart <adrian.burkhart@gmail.com>
2023-03-01 12:53:39 +0100

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,25 @@
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
+
+// https://astro.build/config
+import tailwind from '@astrojs/tailwind'
+
+// https://astro.build/config
+import sitemap from '@astrojs/sitemap'
+
+// https://astro.build/config
+
+// https://astro.build/config
+import netlify from '@astrojs/netlify/functions'
+
+// https://astro.build/config
+import mdx from '@astrojs/mdx'
+
+// https://astro.build/config
+export default defineConfig({
+  // Enable React to support React JSX components.
+  site: 'https://adrian-burkhart.de',
+  integrations: [react(), tailwind(), sitemap(), mdx()],
+  output: 'server',
+  adapter: netlify(),
+})
